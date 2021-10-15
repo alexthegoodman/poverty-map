aa8d00efee9b022cab48481d3addd074c678622a
Alex Woodman <alexthegoodman@gmail.com>
2021-10-14 18:49:44 -0700

    init poverty-map-api

--- packages/poverty-map-api/__tests__/poverty-map-api.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const povertyMapApi = require('..');
-
-describe('poverty-map-api', () => {
-    it('needs tests');
-});
--- packages/poverty-map-api/lib/poverty-map-api.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = povertyMapApi;
-
-function povertyMapApi() {
-    // TODO
-}
